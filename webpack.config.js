a4af0ced97b105e45f4a7c15fdcb9bf888f0fab5
lestermaudiel <flestermaudiel2130@gmail.com>
2023-08-27 16:58:31 -0600

    se agregaron rutas para webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     'js/inicio' : './src/js/inicio.js',
     'js/login/index' : './src/js/login/index.js',
     'js/productos/index' : './src/js/productos/index.js',
+    'js/usuarios/index' : './src/js/usuario/index.js',
     'js/productos/estadistica' : './src/js/productos/estadistica.js',
   },
   output: {
