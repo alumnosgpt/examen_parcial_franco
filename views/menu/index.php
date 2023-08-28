9b705cf41a34b50b2c9fa0ff60006fac2c0e51ea
lestermaudiel <flestermaudiel2130@gmail.com>
2023-08-28 07:57:20 -0600

    Semodifico el index para menu

--- views/menu/index.php
+++ views/menu/index.php
@@ -11,10 +11,9 @@
   <div class="container">
     <div class="menu-container">
       <h1 class="menu-title">MENU PRINCIPAL</h1>
-      <a href="/examen_parcial_franco/productos/datatable" class="btn btn-link menu-link">DATATABLE PRODUCTOS</a>
-      <a href="/examen_parcial_franco/productos/estadistica" class="btn btn-link menu-link">ESTADISTICA PRODUCTOS</a>
-      <a href="/examen_parcial_franco/permisos" class="btn btn-link menu-link">USUARIOS</a>
-      <a href="/examen_parcial_franco/roles" class="btn btn-link menu-link">roles</a>
+      <!-- <a href="/examen_parcial_franco/productos/datatable" class="btn btn-link menu-link">DATATABLE PRODUCTOS</a>
+      <a href="/examen_parcial_franco/productos/estadistica" class="btn btn-link menu-link">ESTADISTICA PRODUCTOS</a> -->
+      <a href="/examen_parcial_franco/permisos" class="btn btn-link menu-link">LISTADO Y ADMINISTRACION DE USUARIOS</a>
 
       <a href="/examen_parcial_franco/estadistica1" class="btn btn-link menu-link">ESTADISTICA PERSONAS ROLES</a>
       <a href="/examen_parcial_franco/estadistica2" class="btn btn-link menu-link">ESTADISTICA ACTIVOS E INACTIVOS</a>
