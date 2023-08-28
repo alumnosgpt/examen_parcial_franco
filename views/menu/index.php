ca37d6aa126f3a5d2ec5720539a058a5af9d4cc7
lestermaudiel <flestermaudiel2130@gmail.com>
2023-08-27 20:48:05 -0600

    se modifico el index. para poder tener la ruta correcta.

--- views/menu/index.php
+++ views/menu/index.php
@@ -3,16 +3,21 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="styles.css">
+  <link rel="stylesheet" href="<?= asset('./build/css/styles.css') ?>"> 
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <title>Menú Principal</title>
 </head>
 <body>
-  <div class="menu-container">
-    <h1 class="menu-title">MENU PRINCIPAL</h1>
-    <a href="/examen_parcial_franco/productos/datatable" class="menu-link">DATATABLE PRODUCTOS</a>
-    <a href="/examen_parcial_franco/productos/estadistica" class="menu-link">ESTADISTICA PRODUCTOS</a>
-    <a href="/examen_parcial_franco/permisos/datatable" class="menu-link">USUARIOS</a>
+  <div class="container">
+    <div class="menu-container">
+      <h1 class="menu-title">MENU PRINCIPAL</h1>
+      <a href="/examen_parcial_franco/productos/datatable" class="btn btn-link menu-link">DATATABLE PRODUCTOS</a>
+      <a href="/examen_parcial_franco/productos/estadistica" class="btn btn-link menu-link">ESTADISTICA PRODUCTOS</a>
+      <a href="/examen_parcial_franco/permisos/datatable" class="btn btn-link menu-link">USUARIOS</a>
+    </div>
+    <div class="text-center mt-3">
+      <a href="/examen_parcial_franco/logout" class="btn logout-btn">Cerrar sesión</a>
+    </div>
   </div>
-  <a href="/examen_parcial_franco/logout" class="logout-btn">Cerrar sesión</a>
 </body>
 </html>
