e1885c325ffa291d9f994af71133cfae4dbd47a3
lestermaudiel <flestermaudiel2130@gmail.com>
2023-08-27 20:08:57 -0600

    Se agrego boton para poder ingresar a los usuarios.

--- views/menu/index.php
+++ views/menu/index.php
@@ -1,4 +1,18 @@
-<h1>menu principal</h1>
-<a href="/examen_parcial_franco/productos/datatable" class="btn btn-link">Datatable</a>
-<a href="/examen_parcial_franco/productos/estadistica" class="btn btn-link">Estadistica</a>
-<a href="/examen_parcial_franco/logout" class="btn btn-danger">Cerrar sesión</a>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <title>Menú Principal</title>
+</head>
+<body>
+  <div class="menu-container">
+    <h1 class="menu-title">MENU PRINCIPAL</h1>
+    <a href="/examen_parcial_franco/productos/datatable" class="menu-link">DATATABLE PRODUCTOS</a>
+    <a href="/examen_parcial_franco/productos/estadistica" class="menu-link">ESTADISTICA PRODUCTOS</a>
+    <a href="/examen_parcial_franco/permisos/datatable" class="menu-link">USUARIOS</a>
+  </div>
+  <a href="/examen_parcial_franco/logout" class="logout-btn">Cerrar sesión</a>
+</body>
+</html>
